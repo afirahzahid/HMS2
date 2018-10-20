b1d1e0ad7449f59dcdbddf3f6959135c9bae5c23
afirahzahid <43724763+afirahzahid@users.noreply.github.com>
2018-10-20 06:03:57 -0700

    Add files via upload

---
+++ NewHMS3/Controllers/RtLogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HostelManagementSystem.Controllers
+{
+    public class RtLogController : Controller
+    {
+        // GET: RtLog
+        public ActionResult RtLogForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ NewHMS3/Controllers/StaffLogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HostelManagementSystem.Controllers
+{
+    public class StaffLogController : Controller
+    {
+        // GET: StaffLog
+        public ActionResult StaffLogForm()
+        {
+            return View();
+        }
+    }
+}
